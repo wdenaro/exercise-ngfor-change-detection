813b87ef159e98293e4cad8330291961567572a7
Wesley Denaro <Wesley.Denaro@iCloud.com>
2018-03-15 10:51:34 -0400

    Iterate through array of objects with Add/Remove functionallity. Initial Commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  headingText = 'Exercise – ngFor Change Detection';
+  courses = [
+    { name: 'Comparative History of Organized Crime', location: 'Williams College' },
+    { name: 'The Amazing World of Bubbles', location: 'Cal-Tech' },
+    { name: 'The Anthropology of Computing:', location: 'MIT' },
+    { name: 'Philosophy and Star Trek', location: 'Georgetown' },
+    { name: 'Underwater Basket Weaving', location: 'Reed College' }
+  ];
+
+  onAdd() {
+    this.courses.push({ name: 'New Course', location: 'College/University' });
+  }
+
+  onRemove(course) {
+    const index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
 }
